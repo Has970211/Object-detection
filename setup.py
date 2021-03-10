b416f275a4e4269ded6d2f8c751809759085516c
Has970211 <68479100+Has970211@users.noreply.github.com>
2021-03-10 09:56:15 +0530

    repo change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="hasara, shehan, asitha, bimsara",
     author_email='',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/Has970211/Object_Detection",
+    url="https://github.com/Has970211/Object-detection",
     install_requires=['torch', 'torchvision', 'cython', 'pyyaml', 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI', 'detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.7/index.html'],
     packages=find_packages()
 
