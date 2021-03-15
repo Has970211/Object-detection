a916cd850dabffd6389c48bad0d3c83cc25228ab
Hasara Koralege <68479100+Has970211@users.noreply.github.com>
2021-03-16 01:57:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name='ObjectDetection_detectron2',
     version='1.0.0',
     description='Object detection project for cognite',
-    author="hasara, shehan, asitha, bimsara",
+    author="hasara, asitha, bimsara, shehan",
     author_email='',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url="https://github.com/Has970211/Object_Detection",
-    install_requires=['torch', 'torchvision', 'PyYAML'],
-    packages=find_packages()
+    #scripts = ['scripts/inference.py'],
+    install_requires=['torch', 'torchvision', 'PyYAML', 'Pillow'],
+    packages=find_packages(),
+
 )
