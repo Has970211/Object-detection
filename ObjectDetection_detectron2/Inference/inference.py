03ee5153e1996caf730073122c9d72a7fb9ffc74
Hasara Koralege <68479100+Has970211@users.noreply.github.com>
2021-03-16 01:18:43 +0530

    Update inference.py

--- ObjectDetection_detectron2/Inference/inference.py
+++ ObjectDetection_detectron2/Inference/inference.py
@@ -10,26 +10,30 @@ from detectron2.utils.visualizer import ColorMode
 from ObjectDetection_detectron2.Dataloader.dataloader import dataloader
 
 class test(object):
-    def __init__(self, OUTPUT_DIR,data_dir,json_dir,img_dir):
+    def __init__(self, OUTPUT_DIR,data_dir,json_dir,img_dir, threshold_scr):
        self.OUTPUT_DIR=OUTPUT_DIR
        self.data_dir=data_dir
        self.json_dir=json_dir
        self.img_dir=img_dir
+       self.threshold_scr = threshold_scr
 
     def call(self):
         cfg = get_cfg()
         cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")  # Let training initialize from model zoo
-        cfg.MODEL.WEIGHTS = os.path.join(self.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.75  #set a custom testing threshold
+        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 7 
+        cfg.OUTPUT_DIR=self.OUTPUT_DIR
+        
+        cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = self.threshold_scr  #set a custom testing threshold
         predictor = DefaultPredictor(cfg)
 
-        for d in ["val"]:
-            DatasetCatalog.register("dataset_"+d, lambda d=d: dataloader.get_dataset_dicts(data_dir+d,json_dir, img_dir))
-            MetadataCatalog.get("dataset_"+d).set(thing_classes=['gauge', 'valve', 'isolation', 'tank', 'idtag', 'pump', 'gaugeD'])
-        
+        for d in ['val']:
+            DatasetCatalog.register("dataset_val", lambda d=d: dataloader.get_dataset_dicts(self.data_dir, self.json_dir, self.img_dir))
+            MetadataCatalog.get("dataset_val").set(thing_classes=['gauge', 'valve', 'isolation', 'tank', 'idtag', 'pump', 'gaugeD'])
         custom_metadata = MetadataCatalog.get("dataset_val")
-        dataset_val=dataloader.get_dataset_dicts(self.data_dir+d,self.json_dir, self.img_dir)
+        dataset_val=dataloader.get_dataset_dicts(self.data_dir+d,self.json_dir, img_dir)
  
         for d in random.sample(dataset_val, 16):
             im = cv2.imread(d["file_name"])
@@ -42,6 +46,7 @@ class test(object):
 
             out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
             cv2.imshow(out.get_image()[:, :, ::-1])
+
 
 
 
