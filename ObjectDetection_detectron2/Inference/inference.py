6a813e357a9ac0b063ab0f77ac3a5d48b0e9b895
Hasara Koralege <68479100+Has970211@users.noreply.github.com>
2021-03-16 14:03:05 +0530

    Update inference.py

--- ObjectDetection_detectron2/Inference/inference.py
+++ ObjectDetection_detectron2/Inference/inference.py
@@ -5,7 +5,7 @@ from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
+from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.utils.visualizer import ColorMode
 from ObjectDetection_detectron2.Dataloader.dataloader import dataloader
 
