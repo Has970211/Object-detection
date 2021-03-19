530b7b9137d8c8dd449ff27a70c80274bd3c1341
Hasara Koralege <68479100+Has970211@users.noreply.github.com>
2021-03-19 11:03:10 +0530

    Update CREATE_D.py

--- ObjectDetection_detectron2/CREATED/CREATE_D.py
+++ ObjectDetection_detectron2/CREATED/CREATE_D.py
@@ -6,20 +6,19 @@ class CreateD(object):
         l=self.outputs['instances']
         dict1={}
         for i in range(len(l)):
-          d={}
-          a=l[i].get_fields()
+            d={}
+            a=l[i].get_fields()
+            z=tuple(a['pred_boxes'])
+            z=z[0].tolist()
+            d['pred_box']=z
+            
+            x=a['scores']
+            x=x.tolist()
+            d['score']=x[0]
 
-          z=tuple(a['pred_boxes'])
-          z=z[0].tolist()
-          d['pred_box']=z
-
-          x=a['scores']
-          x=x.tolist()
-          d['score']=x[0]
-
-          y=a['pred_classes']
-          y=y.tolist()
-          d['pred_class']=y[0]
-
-          dict1[str(i)]=d
+            y=a['pred_classes']
+            y=y.tolist()
+            d['pred_class']=y[0]
+            
+            dict1[str(i)]=d
          return dict1
